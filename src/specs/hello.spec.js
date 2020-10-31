e2e04d26e9f7823dfbdac71cb4c6c5b735c32da4
Brian Gershon <brian.five@gmail.com>
2020-10-31 10:29:14 -0700

    Add prettier config

--- src/specs/hello.spec.js
+++ src/specs/hello.spec.js
@@ -2,5 +2,7 @@ import getMessage from '../hello';
 
 test('has default message', () => {
   const message = getMessage();
-  expect(message).toEqual('Welcome to your Vanilla JavaScript site, built with minimal dependencies.');
+  expect(message).toEqual(
+    'Welcome to your Vanilla JavaScript site, built with minimal dependencies.'
+  );
 });
